c37c865d16ddf632f42258a1aebcf540f72a263e
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-08-07 14:49:12 +0530

    added: rollDie function to get random values and pass that value to the position

--- snakeAndLadder.js
+++ snakeAndLadder.js
@@ -1,4 +1,12 @@
 console.log("Welcome to snake and ladder problem")
 
-let playerPosition = 0;
-console.log(`Game start at position: ${playerPosition}`);
+let position = 0;
+/**
+ * diceNumber is taking random value from 1 to 6
+ */
+function rollDice(){
+    let diceNumber = (Math.floor(Math.random()*10)%6+1);
+    return diceNumber;
+}
+position = position+rollDice();
+console.log(`Player got the position: ${position}`);
